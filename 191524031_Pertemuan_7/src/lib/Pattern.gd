8cb26463aa0dedb04427b923f210538a1eecbf3b
reyrizki <reynaldirizki97.rr@gmail.com>
2021-05-28 22:04:27 +0700

    chore(Pattern): Add first-half animation
    - Animate square
    - Center animation
    - Change legacy code
    - Swap center and point variable in rotate_point

--- 191524031_Pertemuan_7/src/lib/Pattern.gd
+++ 191524031_Pertemuan_7/src/lib/Pattern.gd
@@ -1,16 +1,40 @@
-extends Node
+extends Transformations
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+class_name Pattern
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var steps = 0
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	steps += 1
+
+	if (steps == 180):
+		steps = 0
+
+	update()
+
+
+func draw_filled_square(current: Vector2, color: Color, degree: float):
+	var colors = PoolColorArray([color])
+	var scale_factor = steps * 0.1
+
+	var points = PoolVector2Array([
+		rotate_point(Vector2(current.x, current.y) + scale_factor * Vector2(0, -1), current, degree + steps),
+		rotate_point(Vector2(current.x, current.y) + scale_factor * Vector2(1, 0), current, degree + steps),
+		rotate_point(Vector2(current.x, current.y) + scale_factor * Vector2(0, 1), current, degree + steps),
+		rotate_point(Vector2(current.x, current.y) + scale_factor * Vector2(-1, 0), current, degree + steps),
+	])
+
+	draw_polygon(points, colors)
+
+func draw_v():
+	pass
+
+func draw_pattern(start, color, degree):
+	var current = Vector2(start.x + steps * sin(deg2rad(360 - degree)), start.y + steps * cos(deg2rad(degree)))
+	if(steps < 90):
+		draw_filled_square(current, color, degree)
+	else:
+		draw_v()
--- 191524031_Pertemuan_7/src/scenes/Pattern.gd
+++ 191524031_Pertemuan_7/src/scenes/Pattern.gd
@@ -1,50 +1,18 @@
-extends Node2D
-
-
-var angle_from: int = 30
-var radius: int = 0
-var reverse: bool = false
-var x: int = 0
-var y: int = 50
-
-
-func _process(delta):
-	angle_from += 1
-	radius += (-1 if reverse else 1) * 1
-	
-	var width: int = get_viewport().size.x
-	var height: int = get_viewport().size.x
-	x += 1
-	
-	if (x == width):
-		y += 20
-		x = 0
-		
-		if (y >= height):
-			y = 0
-	
-	if (radius == 0):
-		reverse = false
-	elif (radius == 50):
-		reverse = true
-	
-	update()
-
-
-func draw_circle_arc_poly(center, radius, angle_from, angle_to, color):
-	var nb_points = 32
-	var points_arc = PoolVector2Array()
-	points_arc.push_back(center)
-	var colors = PoolColorArray([color])
-
-	for i in range(nb_points + 1):
-		var angle_point = deg2rad(angle_from + i * (angle_to - angle_from) / nb_points - 90)
-		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
-	draw_polygon(points_arc, colors)
+extends Pattern
 
 
 func _draw():
-	draw_circle_arc_poly(Vector2(x, y), radius, angle_from, angle_from + 300, Color(1, 1, 1))
+	var max_size = get_viewport().size
+	var center = max_size / 2
+
+	var tip = center + Vector2(0, -200)
+
+	for i in range(8):
+		var degree = i * 45
+
+		var start = rotate_point(tip, center, degree)
+
+		draw_pattern(start, Color(0, 0, 1), degree)
 
 
 func _on_Button_pressed():
